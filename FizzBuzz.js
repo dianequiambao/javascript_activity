9cd1de608efb18e01295b16e927c77923019a446
dianequiambao <126642728+dianequiambao@users.noreply.github.com>
2025-01-14 08:29:35 +0800

    Add files via upload
    Generating FizzBuzz using 'while loop'

---
+++ FizzBuzz.js
@@ -0,0 +1,22 @@
+
+var output = [];
+var count = 0;
+
+function fizzBuzz(){
+
+
+    while (count <= 100){
+    if (count == 0){
+        output.push(count);
+    } else if (count = 1){
+        output.push(count);
+    } else{
+        output.push(count);
+    }
+    count++;
+    
+    console.log(output);
+    }
+    
+}
+
